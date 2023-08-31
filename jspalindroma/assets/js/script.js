138e79a859f79a93f673027e9f3a8a1a06e325b9
RiccardoSansone <r.sansone@gmail.com>
2023-08-31 17:13:30 +0200

    complete

--- jspalindroma/assets/js/script.js
+++ jspalindroma/assets/js/script.js
@@ -1,3 +1,18 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola 
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+const control = document.querySelector('button');
+control.addEventListener('click', function(){
+    let word = document.getElementById('parola').value;
+    let lastletter = word.length - 1;
+    let result = 'la parola é palindroma';
+    for(let i = 0; i < Math.floor(word.length/2); i++ ){
+        if(word[i] != word[lastletter]){
+            result ='la tua parola non é palindroma' 
+        }
+        lastletter--;
+    }
+    console.log(result);
+})
