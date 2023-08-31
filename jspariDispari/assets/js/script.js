7a04121de29137948b5ab8a38f36fa993d3bc42b
RiccardoSansone <r.sansone@gmail.com>
2023-08-31 16:44:03 +0200

    complete

--- jspariDispari/assets/js/script.js
+++ jspariDispari/assets/js/script.js
@@ -3,3 +3,35 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 // Dichiariamo chi ha vinto.
+
+let sceltaUt = document.getElementById('numero');
+let pariUt = document.getElementById('pari');
+let dispariUt = document.getElementById('dispari');
+let pariDispari; //true se è pari, false se è dispari
+
+pariUt.addEventListener('click', function () {
+    if(result(true) == true){
+        console.log('hai vinto');
+    } else{
+        console.log('hai perso');
+    }
+})
+
+dispariUt.addEventListener('click', function () {
+    if(result(false) == true){
+        console.log('hai vinto');
+    } else {
+        console.log('hai perso');
+    }
+})
+
+function result (pariDispari){
+    let pcNumber = Number(Math.floor(Math.random()* 5) + 1);
+    let randomNum = pcNumber + sceltaUt.value;
+    if(pariDispari == true && randomNum% 2 == 0){
+        return true;
+    } else if(pariDispari == false && randomNum% 2 != 0){
+       return true; 
+    }
+    return false;
+}
